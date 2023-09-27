36d455ec3469e1a55c2fc0fd0142a2e2546c1a89
shekinah <nyamburamary1212@gmail.com>
2023-09-27 21:16:49 +0300

    tasklister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+document.addEventListener("DOMContentLoaded" ,() => {
+let form = document.querySelector("form")
+form.addEventListener("submit", (e) => {
+  e.preventDefault()
+  let value = e.target.name.value;
+  let namesDiv = document.querySelector("#names-container")
+  let p = document.createElement("p")
+  p.textContent = `${value}`
+  p.style.textAlign = "center"
+  let button = document.createElement("button")
+  button.textContent = `Remove Student`
+  button.addEventListener("click",(e) => {
+    e.target.parentNode.remove()
+  })
+  p.appendChild(button)
+  namesDiv.appendChild(p)
+  form.reset()  
+  })
+})
+
+
+
+
+//document.addEventListener("DOMContentLoaded", () => {
+//let form = document.querySelector('form')
+//form.addEventListener('submit', (e) => {
+  //  e.preventDefault()
+    //buildToDo(e.target.my-todos.value)
+    //form.reset()
+  //})
+//})
+//function buildToDoTodos(todos){
+  //let p = document.createElement('p')
+  //let btn = document.createElement('button')
+  //btn.addEventListener('click', handleDelete)
+  //btn.textContent = 'x'
+  //p.textContent = `${todos}`
+  //p.appendChild(btn)
+  //console.log(p)
+  //document.querySelector('#My-todos').appendchild(p)
+//}
+
+//function handleDelete(e){
+//  e.target.parentNode.remove()
+//}
