b8c62d119d720bcd91efd26ddf9b779b74e28544
shekinah <nyamburamary1212@gmail.com>
2023-09-27 23:06:54 +0300

    tasklister

--- src/index.js
+++ src/index.js
@@ -5,28 +5,19 @@ document.addEventListener("DOMContentLoaded", () => {
   form.addEventListener("submit", (e) => {
     e.preventDefault();
     
-    // Get the value from the input field
     const inputValue = document.querySelector("#new-task-description").value;
 
-    // Create a new list item
     const listItem = document.createElement("li");
     listItem.textContent = inputValue;
 
-    // Create a remove button
     const removeButton = document.createElement("button");
     removeButton.textContent = "Remove Task";
     removeButton.addEventListener("click", (e) => {
-      // Remove the parent <li> element when the button is clicked
       listItem.remove();
     });
 
-    // Append the remove button to the list item
     listItem.appendChild(removeButton);
-
-    // Append the list item to the tasks list
     tasksList.appendChild(listItem);
-
-    // Reset the form input
     form.reset();
   });
 });
